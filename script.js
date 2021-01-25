ee3a70561f959d62274a0b17b9d80e47fcce6ff1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-01-25 12:45:41 +0100

    Implement sticky navigation

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
+const header = document.querySelector('.header');
+
 //////////////////////////////////////////////
 // Modal Window
 //////////////////////////////////////////////
@@ -111,3 +113,24 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//////////////////////////////////////////////
+// Sticky Navigation
+//////////////////////////////////////////////
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
